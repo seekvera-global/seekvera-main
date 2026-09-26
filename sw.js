505b0f3b56b9dc9e83aceedb1105264980f4f4eb
seekvera-global <seekvera.global@gmail.com>
2026-09-26 07:15:32 -0700

    R74: update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const RELEASE='20260926-r73-true-multilingual-voice';
+const RELEASE='20260926-r74-global-ai-failover';
 self.addEventListener('install',e=>{e.waitUntil(self.skipWaiting())});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{for(const k of await caches.keys())await caches.delete(k);await self.clients.claim()})())});
 self.addEventListener('message',e=>{if(e.data==='SKIP_WAITING')self.skipWaiting()});
